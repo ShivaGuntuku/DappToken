a520d63de712533e7dd948c94cb5f7897d896ab0
ShivaGuntuku <guntuku.shiva@hotmail.com>
2018-06-16 13:49:58 +0530

    truffle inital setup and contract creation

---
+++ contracts/DappToken.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.2;
+
+
+/**
+ * The DappToken contract does this and that...
+ */
+contract DappToken {
+    // Contrstructor
+    // Set the total Token   
+    // Read total number of Token
+    uint256 public totalSupply;
+
+    constructor DappToken() public {
+        totalSupply = 1000000;
+    }
+}   
