b03a9243ab106b7f460bd47a4c38385679da7cff
ShivaGuntuku <guntuku.shiva@hotmail.com>
2018-06-16 21:14:20 +0530

    Delegated Transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -16,9 +16,19 @@ contract DappToken {
         address indexed _to,
         uint256 _value
     );
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+    
     
 
     mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    
     
 
     constructor(uint256 _initialSupply) public {
@@ -26,7 +36,7 @@ contract DappToken {
         totalSupply = _initialSupply;
     }
 
-    // transfer function
+    // transfer function allow DappToken to other address
     function transfer(address _to, uint256 _value) public returns(bool success) {
         require (balanceOf[msg.sender] >= _value);
         balanceOf[msg.sender] -= _value;
@@ -36,5 +46,25 @@ contract DappToken {
         
         return true;
     }
+
+    //approve Function
+    function approve(address _spender, uint256 _value) public returns(bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+    
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
+        require (_value <= balanceOf[_from]);
+        require (_value <= allowance[_from][msg.sender]);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+    
     
 }   
